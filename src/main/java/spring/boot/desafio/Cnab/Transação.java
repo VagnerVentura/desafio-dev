dfc9efaa8f17ee7c976ffb632272bbb0de704cb2
vagner ventura <vagnerventura@hotmail.com>
2025-04-03 11:43:49 -0300

    Primeiro commit - adicionando arquivos iniciais

---
+++ src/main/java/spring/boot/desafio/Cnab/Transação.java
@@ -0,0 +1,12 @@
+package spring.boot.desafio.Cnab;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table
+public class Transação {
+
+	private String tipo;
+	
+}
---
+++ src/test/java/spring/boot/desafio/Cnab/CnabApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.boot.desafio.Cnab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CnabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
